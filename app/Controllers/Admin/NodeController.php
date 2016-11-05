bc70703e8dee614a16b9d117fba978ade528fdba
root <admin@bydisk.com>
2016-11-05 08:45:12 +0800

    updatee

--- app/Controllers/Admin/NodeController.php
+++ app/Controllers/Admin/NodeController.php
@@ -31,7 +31,7 @@ class NodeController extends AdminController
         $node->status = $request->getParam('status');
         $node->sort = $request->getParam('sort');
         $node->level = $request->getParam('level');
-        $node->group = $request->getParam('group');
+        $node->g = $request->getParam('group');
         if (!$node->save()) {
             $rs['ret'] = 0;
             $rs['msg'] = "添加失败";
@@ -67,7 +67,7 @@ class NodeController extends AdminController
         $node->status = $request->getParam('status');
         $node->sort = $request->getParam('sort');
         $node->level = $request->getParam('level');
-        $node->group = $request->getParam('group');
+        $node->g = $request->getParam('group');
         if (!$node->save()) {
             $rs['ret'] = 0;
             $rs['msg'] = "修改失败";
