b59293bb1575e870ba4cb28d0e84b2d11505bcea
Lan672111 <lan894734188@gmail.com>
2016-11-19 17:09:27 +0800

    update

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -12,6 +12,7 @@ use App\Services\DbConfig;
 use App\Utils\Hash;
 use App\Utils\Tools;
 
+use App\Utils\GA;
 
 /**
  *  HomeController
@@ -238,6 +239,66 @@ class UserController extends BaseController
         $res['ret'] = 1;
         return $this->echoJson($response, $res);
     }
+    ### GA ###
+    public function GaCheck($request, $response, $args)
+    {
+        $code = $request->getParam('code');
+        $user = $this->user;
+        
+
+        
+        if ( $code == "") {
+            $res['ret'] = 0;
+            $res['msg'] = "悟空别闹";
+            return $response->getBody()->write(json_encode($res));
+        }
+        
+        $ga = new GA();
+        $rcode = $ga->verifyCode($user->ga_token,$code);
+        if (!$rcode) {
+            $res['ret'] = 0;
+            $res['msg'] = "测试错误";
+            return $response->getBody()->write(json_encode($res));
+        }
+        
+        
+        $res['ret'] = 1;
+        $res['msg'] = "测试成功";
+        return $response->getBody()->write(json_encode($res));
+    }
+    public function GaSet($request, $response, $args)
+    {
+        $enable = $request->getParam('enable');
+        $user = $this->user;
+        
+
+        
+        if ( $enable == "") {
+            $res['ret'] = 0;
+            $res['msg'] = "悟空别闹";
+            return $response->getBody()->write(json_encode($res));
+        }
+        
+        $user->ga_enable=$enable;
+        $user->save();
+        
+        
+        $res['ret'] = 1;
+        $res['msg'] = "设置成功";
+        return $response->getBody()->write(json_encode($res));
+    }
+    public function GaReset($request, $response, $args)
+    {
+        $user = $this->user;
+        $ga = new GA();
+        $secret = $ga->createSecret();
+        
+        $user->ga_token=$secret;
+        $user->save();
+        $newResponse = $response->withStatus(302)->withHeader('Location', '/user/edit');
+        return $newResponse;
+    }
+
 
     public function logout($request, $response, $args)
     {
