5980c72dec24c95cc563e5aac0ffaa5cddfdbc9a
批发靓鸡仔 <lan894734188@gmail.com>
2017-01-08 22:13:55 +0800

    Update UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -88,7 +88,7 @@ class UserController extends BaseController
             $json_show = json_encode($ary, JSON_PRETTY_PRINT);
             if ($ctype == "SSR") {
                 $ssurl = $ary['server']. ":" . $ary['server_port'].":".str_replace("_compatible","",$user->protocol).":".$ary['method'].":".str_replace("_compatible","",$user->obfs).":".Tools::base64_url_encode($ary['password'])."&remarks=".Tools::base64_url_encode($node->name);
-                $ssqr = "ssr://" . Tools::base64_url_encode($ssurl);
+                $ssqr = "ssr://" . base64_encode($ssurl);
             }elseif($ctype == "SS"){
                 $ssurl = $ary['method'] . ":" . $ary['password'] . "@" . $ary['server'] . ":" . $ary['server_port'];
                 $ssqr = "ss://" . base64_encode($ssurl);
