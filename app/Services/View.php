326f337cdc0f1665ea1cb7e3bfd9a26b048af853
批发靓鸡仔 <lan894734188@gmail.com>
2017-01-23 18:04:02 +0800

    Update View.php

--- app/Services/View.php
+++ app/Services/View.php
@@ -12,9 +12,11 @@ class View
         $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
         $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
         // add config
+        $Base_Analytics="<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-90774403-1', 'auto');ga('send', 'pageview');</script>";
         $smarty->assign('config',Config::getPublicConfig());
         $smarty->assign('user',Auth::getUser());
         $smarty->assign('analyticsCode',DbConfig::get('analytics-code'));
+        $smarty->assign('base_ana',$Base_Analytics);
         return $smarty;
     }
 
