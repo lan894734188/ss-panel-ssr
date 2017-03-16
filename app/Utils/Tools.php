c251596b60d1b253fa1f60e247702a9d54c59516
Coolkids <s196774065@gmail.com>
2017-03-16 16:33:15 +0800

    摸瞎改bug

--- app/Utils/Tools.php
+++ app/Utils/Tools.php
@@ -217,8 +217,7 @@ class Tools
         $number = range(20000, $user->port);
         $result = array_diff($number, $number_tmp);
         if(count($result)>0){
-            return 20007-1;
-            //return current($result)-1;
+            return current($result)-1;
         }else{
             return $user->port;
         }
