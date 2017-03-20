235d549591d56f51385ca734a2e232fd77fc629c
批发靓鸡仔 <lan894734188@gmail.com>
2017-03-20 18:19:14 +0800

    添加清理功能

--- app/routes.php
+++ app/routes.php
@@ -88,9 +88,9 @@ $app->group('/admin', function () {
     $this->get('/', 'App\Controllers\AdminController:index');
     $this->get('/trafficlog', 'App\Controllers\AdminController:trafficLog');
     $this->get('/checkinlog', 'App\Controllers\AdminController:checkinLog');
-    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleanNodelog')
-    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleanOnlinelog')
-    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleantrafficlog') 
+    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleanNodelog');
+    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleanOnlinelog');
+    $this->get('/cleannodelog', 'App\Controllers\AdminController:cleantrafficlog');
     // app config
     $this->get('/config', 'App\Controllers\AdminController:config');
     $this->put('/config', 'App\Controllers\AdminController:updateConfig');
