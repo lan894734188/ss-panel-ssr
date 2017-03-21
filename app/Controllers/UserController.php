b237f0f26862279fbe0df8ade19e4ba55b48b1d8
批发靓鸡仔 <lan894734188@gmail.com>
2017-03-21 17:43:55 +0800

    Update UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -38,7 +38,7 @@ class UserController extends BaseController
     {
         $user_index_msg = DbConfig::get('user-index');
         $othershop = Config::get('ManyShopGroup');
-        if($user->g == $othershop){
+        if($this->user->g == $othershop){
             $user_index_topmsg = DbConfig::get('user-index-top-other');
         }else{
             $user_index_topmsg = DbConfig::get('user-index-top');
