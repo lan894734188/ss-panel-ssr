39167d11b70129e7a3538a632eb22dc51a72234a
批发靓鸡仔 <lan894734188@gmail.com>
2017-04-17 20:26:42 +0800

    Update Node.php

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -26,7 +26,7 @@ class Node extends Model
     
     public function getNodeError()
     {
-        $errorcheck = $this->getLastNodeInfolog()->uptime;
+        $errorcheck = (int)($this->getLastNodeInfolog()->uptime);
         if((time()-$errorcheck)>600 || $errorcheck = null){
             return "false";
         }
