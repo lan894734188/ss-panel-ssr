06a9aae47c1381daef6335ff3e0adc533ea44aa7
批发靓鸡仔 <lan894734188@gmail.com>
2017-07-21 17:31:47 +0800

    Update ApiController.php

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -111,7 +111,7 @@ class ApiController extends BaseController
 	$user_level = $userauth->level;
 	$user_g = $userauth->g;
 	##$nodes = Node::where('type', '1')->where(function ($query){$query->where('g', $user_g)->orwhere('g', '0');})->orderBy('sort')->get();
-	$nodes = Node::get();
+	$nodes = Node::where('type', '1')->get();
 	$rss_link = "";
 	$arys['server'] = $nodes->server;
         $arys['server_port'] = $userauth->port;
